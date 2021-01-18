be0e132dfd5c7a6be8d5a16d767cb0bdbfd4e689
caneleex <git@mlnr.dev>
2021-01-18 16:37:09 +0100

    got lavamark to work

--- src/main/java/com/frederikam/lavamark/Lavamark.java
+++ src/main/java/com/frederikam/lavamark/Lavamark.java
@@ -40,8 +40,8 @@ public class Lavamark {
     private static final Logger log = LoggerFactory.getLogger(Lavamark.class);
 
     static final AudioPlayerManager PLAYER_MANAGER = new DefaultAudioPlayerManager();
-    private static final String DEFAULT_PLAYLIST = "https://www.youtube.com/watch?v=7v154aLVo70&list=LLqqLoSLryroL7b7TAL8gfhQ&index=22";
-    private static final String DEFAULT_OPUS = "https://www.youtube.com/watch?v=M_36UBLkni8";
+    private static final String DEFAULT_PLAYLIST = "https://www.youtube.com/watch?v=dcGKVnEbi8E&list=PLdp3KzF76wW-O0C6sP8iFPvwZf6HwIZdh";
+    private static final String DEFAULT_OPUS = "https://youtu.be/dcGKVnEbi8E";
     private static final long INTERVAL = 2 * 1000;
     private static final long STEP_SIZE = 20;
     private static final Object WAITER = new Object();
